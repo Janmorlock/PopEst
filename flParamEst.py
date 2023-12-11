d253749521e2298da3f39a31ece8a226bb218490
Jan <naj-kcolrom@web.de>
2023-12-11 15:21:58 +0000

    new e1 model

--- flParamEst.py
+++ flParamEst.py
@@ -20,35 +20,39 @@ if __name__ == "__main__":
 
     param = ModParam()
     if param.mcmc:
-        param.gr_fp = np.random.uniform(0, 0.35, (param.n_samples,cbParam.n_reactors)).T
-        param.min_fl = np.random.uniform(0, [cbData.fl[r][0] for r in range(cbParam.n_reactors)], (param.n_samples,cbParam.n_reactors)).T
+        param.gr_fp = np.random.uniform(0, 7000, (param.n_samples,cbParam.n_reactors)).T
+        param.e1_ofs = np.random.uniform(0, [cbData.fl[r][0]*cbData.b1[r][0] for r in range(cbParam.n_reactors)], (param.n_samples,cbParam.n_reactors)).T
+        param.od_fac = np.random.uniform(0, 1000, (param.n_samples)).T
         # param.od_ofs = np.random.uniform(0.05, 0.3, (n_samples))
 
     # SIMULATE
-    sim_hrs, cb_dil, fl_init, sim_puti = [], [], [], []
-    sim_fl_train, sim_fl_test = [], []
+    sim_hrs, cb_dil, fp_init, sim_puti = [], [], [], []
+    sim_e1_train, sim_e1_test = [], []
     s_min = np.zeros(cbParam.n_reactors, dtype=int)
     for j in range(cbParam.n_reactors):
         sim_hrs.append(np.arange(0,cbData.time_h[j][-1]-cbData.time_h[j][0],param.Ts/3600))
         dil = np.diff(cbData.p1[j], prepend=[0,0]) > 0.015
         cb_dil.append(dil[:-1])
         sim_puti.append(interpolateCbToSim(cbData.time_h[j], cbData.od[j], sim_hrs[j]))
-        fl_init.append((cbData.fl[j][0] - param.min_fl[j])*(cbData.od[j][0]+param.od_ofs))
+        fp_init.append(cbData.fl[j][0]*cbData.b1[j][0] - param.e1_ofs[j] - param.od_fac*cbData.od[j][0])
 
     if param.mcmc:
         for j in range(cbParam.n_reactors):
             print("Training at temperature: ", cbParam.titles[j])
-            sim_fp = simulateFlProtein(fl_init[j], cbData.time_h[j], sim_hrs[j], cbData.temp[j], sim_puti[j], cb_dil[j], j, param).T
-            sim_fl_train.append(((sim_fp/(cbData.od[j]+np.full((len(cbData.od[j]),param.n_samples),param.od_ofs).T)).T+param.min_fl[j]).T)
-            rmse = np.sqrt(np.mean((sim_fl_train[j] - cbData.fl[j])**2,axis=1))
+            sim_fp = simulateFlProtein(fp_init[j], cbData.time_h[j], sim_hrs[j], cbData.temp[j], sim_puti[j], cb_dil[j], j, param).T
+            sim_e1_train.append((sim_fp.T + param.e1_ofs[j] + np.outer(cbData.od[j],param.od_fac)).T)
+            rmse = np.sqrt(np.mean((sim_e1_train[j] - cbData.fl[j]*cbData.b1[j])**2,axis=1))
             s_min[j] = np.argmin(rmse)
 
         param.gr_fp = np.array([param.gr_fp[j][s_min[j]] for j in range(cbParam.n_reactors)])
-        param.min_fl = np.array([param.min_fl[j][s_min[j]] for j in range(cbParam.n_reactors)])
+        param.e1_ofs = np.array([param.e1_ofs[j][s_min[j]] for j in range(cbParam.n_reactors)])
+        param.od_fac = np.array([param.od_fac[s_min[j]] for j in range(cbParam.n_reactors)])
         print("Growth rates:")
         print(*param.gr_fp, sep = ", ")
-        print("Min Fl:")
-        print(*param.min_fl, sep = ", ")
+        print("e1 ofs:")
+        print(*param.e1_ofs, sep = ", ")
+        print("od fac:")
+        print(*param.od_fac, sep = ", ")
         # print("od_ofs:")
         # print(*[param.od_ofs[s_min[j]] for j in range(n_reactors)], sep = ", ")
 
@@ -78,18 +82,18 @@ if __name__ == "__main__":
         param.Beta_fp = np.array([gr_model2.coefficients[1]])
         param.Gam_fp = np.array([gr_model2.coefficients[0]])
 
-    mcmc = param.mcmc
-    param.mcmc = False
-    print("Running with estimated parameters")
-    for j in range(cbParam.n_reactors):
-        fl_init_j = (cbData.fl[j][0] - param.min_fl[j])*(cbData.od[j][0]+param.od_ofs)
-        sim_fp = simulateFlProtein(fl_init_j, cbData.time_h[j], sim_hrs[j], cbData.temp[j], sim_puti[j], cb_dil[j], j, param).T
-        sim_fl_test.append(((sim_fp/(cbData.od[j]+np.full((len(cbData.od[j]),param.n_samples),param.od_ofs).T)).T+param.min_fl[j]).T)
-    param.mcmc = mcmc
+    # mcmc = param.mcmc
+    # param.mcmc = False
+    # print("Running with estimated parameters")
+    # for j in range(cbParam.n_reactors):
+    #     fl_init_j = (cbData.fl[j][0] - param.e1_ofs[j])*(cbData.od[j][0]+param.od_ofs)
+    #     sim_fp = simulateFlProtein(fl_init_j, cbData.time_h[j], sim_hrs[j], cbData.temp[j], sim_puti[j], cb_dil[j], j, param).T
+    #     sim_e1_test.append(((sim_fp/(cbData.od[j]+np.full((len(cbData.od[j]),param.n_samples),param.od_ofs).T)).T+param.e1_ofs[j]).T)
+    # param.mcmc = mcmc
     
     # ANALYSIS
     n_rows = math.ceil(cbParam.n_reactors/2)
-    fig, ax = plt.subplots(n_rows,2,sharey='row')
+    fig, ax = plt.subplots(n_rows,2)
     fig.set_figheight(n_rows*7)
     fig.set_figwidth(20)
     for j in range(cbParam.n_reactors):
@@ -101,13 +105,13 @@ if __name__ == "__main__":
         ax[r][c].patch.set_visible(False)
 
         axr.plot(cbData.time_h[j],cbData.temp[j],'r',lw=0.7)
-        ax[r][c].plot(cbData.time_h[j],cbData.fl[j],'.g',markersize = 0.8, label = '$fl_{meas}$')
+        ax[r][c].plot(cbData.time_h[j],cbData.fl[j]*cbData.b1[j],'.g',markersize = 0.8, label = '$e1_{meas}$')
         if param.mcmc:
-            ax[r][c].plot(cbData.time_h[j],sim_fl_train[j][0],'k',lw = 0.5, label = '$fl_{sim, train}$', alpha = 0.1)
+            ax[r][c].plot(cbData.time_h[j],sim_e1_train[j][0],'k',lw = 0.5, label = '$e1_{sim, train}$', alpha = 0.1)
             for s in range(1,min(param.n_samples,100)):
-                ax[r][c].plot(cbData.time_h[j],sim_fl_train[j][s],'k',lw = 0.5, alpha = 0.1)
-            ax[r][c].plot(cbData.time_h[j],sim_fl_train[j][s_min[j]],'m', lw = 0.7, label = '$fl_{sim, opt}$')
-        ax[r][c].plot(cbData.time_h[j],sim_fl_test[j][0],'b',lw = 0.7, label = '$fl_{sim, model}$')
+                ax[r][c].plot(cbData.time_h[j],sim_e1_train[j][s],'k',lw = 0.5, alpha = 0.1)
+            ax[r][c].plot(cbData.time_h[j],sim_e1_train[j][s_min[j]],'m', lw = 0.7, label = '$e1_{sim, opt}$')
+        # ax[r][c].plot(cbData.time_h[j],sim_e1_test[j][0],'b',lw = 0.7, label = '$fl_{sim, model}$')
 
         # ax[r][c].plot(cbData.time_h[j],cbData.od[j],'g',lw = 0.5, label = 'p. putida od')
         # ax[r][c].vlines(cbData.time_h[j][cb_dil[j]==1],-2,2,'g',lw = 0.5, alpha=0.5, label = 'p. putida dil')
@@ -115,7 +119,7 @@ if __name__ == "__main__":
 
         ax[r][c].legend(loc="upper left")
         if (c == 0):
-            ax[r][c].set_ylabel("Fluorescense [a.u.]")
+            ax[r][c].set_ylabel("Intensity")
         else:
             axr.set_ylabel('Temperature [°C]', color='r')
             ax[r][c].tick_params(axis='y', labelleft=True)
@@ -123,7 +127,7 @@ if __name__ == "__main__":
             ax[r][c].set_xlabel("Time [h]")
         axr.tick_params(axis='y', color='r', labelcolor='r')
         ax[r][c].set_xlim([sim_hrs[j][0]-0.5,sim_hrs[j][-1]+0.5])
-        ax[r][c].set_ylim([-0.05,0.55])
+        ax[r][c].set_ylim([0,1.1*max(cbData.fl[j]*cbData.b1[j])])
         axr.set_ylim([25,38])
         ax[r][c].set_title(cbParam.titles[j])
     # TODO: Set titles
@@ -131,4 +135,4 @@ if __name__ == "__main__":
     fig.tight_layout()
     if not os.path.isdir(results_dir):
         os.makedirs(results_dir)
-    fig.savefig(results_dir + "/fl_sim_{}.png".format("train" if param.mcmc else "test"))
+    fig.savefig(results_dir + "/fl_sim_e1_{}.png".format("train" if param.mcmc else "test"))
--- popParam.py
+++ popParam.py
@@ -11,20 +11,21 @@ class ModParam:
         self.T_pre_e = 37
         self.T_pre_p = 26
 
-        self.dil_th = 0.54
-        self.dil_amount = 0.11
+        self.dil_th = 0.55
+        self.dil_amount = 0.12
         self.Dil_sp = 0.5
         self.Avg_temp = True
         self.Lag = 3 # hours
         # TODO: Adapt min/max fluorescense values of the respective reactors
-        # self.min_fl = [0.0384, 0.141, 0.112, 0.119, 0.104, 0.13, 0.093, 0.108]
-        self.min_fl = np.array([0.06670286988458429, 0.09954091472779605, 0.01339878054716663, 0.09732706551691282, 0.08975873832801415, 0.1128096218942311, 0.061012361789911165, 0.04765733175961176]).T # [0.037, 0.064, 0.057, 0.058, 0.036, 0.068, 0.064, 0.061]
+        # self.e1_ofs = [0.0384, 0.141, 0.112, 0.119, 0.104, 0.13, 0.093, 0.108]
+        self.e1_ofs = np.array([0.06670286988458429, 0.09954091472779605, 0.01339878054716663, 0.09732706551691282, 0.08975873832801415, 0.1128096218942311, 0.061012361789911165, 0.04765733175961176]).T # [0.037, 0.064, 0.057, 0.058, 0.036, 0.068, 0.064, 0.061]
         self.max_fl = []
         self.minv = 0.059
         self.maxv = 0.290
+        self.od_fac = np.array([1.0])
 
         self.od_ofs = np.array([0.2])
-        self.n_samples = 10000
+        self.n_samples = 50000
         self.mcmc = True
 
         # Linear line fitting to 062_5 data (without 36°C)
